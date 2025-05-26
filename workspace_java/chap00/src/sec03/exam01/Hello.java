422a4bc223eb586713de4edf23274b45e18b40a9
movejoo <rani3065@gmail.com>
2025-05-26 15:46:18 +0900

    no message

---
+++ workspace_java/chap00/src/sec03/exam01/Hello.java
@@ -0,0 +1,29 @@
+package sec03.exam01;
+
+// 한줄 주석
+/* 범위 주석 */
+/*
+ * 여러줄
+ * 주석
+ */
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int x = 2; x < 10; x++)
+		{
+			for(int y = 1; y < 10; y++) 
+			{
+				System.out.println(x + " * " + y + " = " + (x*y));
+			}
+			
+			System.out.println("==========");
+		}
+		
+		System.out.println("Hello, Java!!");
+		
+	}
+
+}
